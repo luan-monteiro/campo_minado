b2b776f5b11e57a9307696a9981752aa16e98246
Luan Correa Monteiro <luan.monteirof@gmail.com>
2020-04-21 17:09:10 -0300

    Added UI elements and setted the board size

---
+++ Assets/Scripts/BreakableTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableTile : MonoBehaviour
+{
+    public int id;
+    public Sprite _1;
+    public Sprite _2;
+    public Sprite _3;
+    public Sprite _4;
+    public Sprite bomb;
+    public Sprite flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
