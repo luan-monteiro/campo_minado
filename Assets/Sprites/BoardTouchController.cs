d1e7c3401b6476704f3d928747d961d32490920d
Luan Correa Monteiro <luan.monteirof@gmail.com>
2020-04-21 01:33:03 -0300

    Added tileset

---
+++ Assets/Sprites/BoardTouchController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Esse script lida com os toques no board
+public class BoardTouchController : MonoBehaviour
+{
+    RaycastHit hit;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Console.WriteLine("Não to batendo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        
+    }
+}
